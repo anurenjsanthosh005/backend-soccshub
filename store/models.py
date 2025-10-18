7e7672ef67118a21cbf97369edceca803e277339
anurenj.santhosh <anurenj.santhosh@safetywhat.com>
2025-10-18 11:02:44 +0530

    Initial commit of Django backend

---
+++ store/models.py
@@ -0,0 +1,12 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class User(AbstractUser):
+    ROLE_CHOICES = (
+        ('admin', 'Admin'),
+        ('customer', 'Customer'),
+    )
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='customer')
+
+    def __str__(self):
+        return f"{self.username} ({self.role})"
